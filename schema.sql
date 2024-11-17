03f2f20e29674e94fc60d8c2b82c1350ee55fb47
Iikka Terho <iikkater@gmail.com>
2024-11-17 23:19:56 +0200

    Add SQL schema

---
+++ schema.sql
@@ -0,0 +1,127 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 16.5 (Ubuntu 16.5-1.pgdg22.04+1)
+-- Dumped by pg_dump version 17.1 (Ubuntu 17.1-1.pgdg22.04+1)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET transaction_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: terhoi
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    username character varying(50) NOT NULL,
+    password character varying(200) NOT NULL,
+    tier character varying(20) DEFAULT 'basic'::character varying NOT NULL,
+    firstname character varying(50),
+    surname character varying(50),
+    email character varying(100)
+);
+
+
+ALTER TABLE public.users OWNER TO terhoi;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: terhoi
+--
+
+CREATE SEQUENCE public.users_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.users_id_seq OWNER TO terhoi;
+
+--
+-- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: terhoi
+--
+
+ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
+
+
+--
+-- Name: users id; Type: DEFAULT; Schema: public; Owner: terhoi
+--
+
+ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Name: users unique_email; Type: CONSTRAINT; Schema: public; Owner: terhoi
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT unique_email UNIQUE (email);
+
+
+--
+-- Name: users unique_username; Type: CONSTRAINT; Schema: public; Owner: terhoi
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT unique_username UNIQUE (username);
+
+
+--
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: terhoi
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: users users_username_key; Type: CONSTRAINT; Schema: public; Owner: terhoi
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_username_key UNIQUE (username);
+
+
+--
+-- Name: SCHEMA public; Type: ACL; Schema: -; Owner: pg_database_owner
+--
+
+GRANT ALL ON SCHEMA public TO terhoi;
+GRANT USAGE ON SCHEMA public TO wos_app;
+
+
+--
+-- Name: TABLE users; Type: ACL; Schema: public; Owner: terhoi
+--
+
+GRANT SELECT,INSERT,DELETE,UPDATE ON TABLE public.users TO wos_app;
+
+
+--
+-- Name: DEFAULT PRIVILEGES FOR TABLES; Type: DEFAULT ACL; Schema: public; Owner: postgres
+--
+
+ALTER DEFAULT PRIVILEGES FOR ROLE postgres IN SCHEMA public GRANT SELECT,INSERT,DELETE,UPDATE ON TABLES TO wos_app;
+
+
+--
+-- PostgreSQL database dump complete
+--
+
